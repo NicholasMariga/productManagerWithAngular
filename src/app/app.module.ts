800faec2e15137e2aa1932b1d8dc70cd7081058d
Nicholas Mariga <dolphnikoz@gmail.com>
2021-10-07 14:19:52 +0300

    learning angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
+import { ProductListComponent } from './products/product-list.component';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductListComponent,
+    ConvertToSpacesPipe
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
